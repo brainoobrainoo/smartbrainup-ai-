fc466c06327c55370d42a6185b56029c7f7d9444
mar paolic <paolichats@gmail.com>
2026-02-18 17:19:45 +0100

    fix: adjust floating button position mobile and desktop

--- components/ui/FloatingChatButton.tsx
+++ components/ui/FloatingChatButton.tsx
@@ -88,8 +88,8 @@ export default function FloatingChatButton({
       onClick={onClick ? (e) => { e.preventDefault(); onClick(); } : undefined}
       style={{
         position: 'fixed',
-        bottom: isMobile ? 'calc(15% + 190px)' : 'calc(18% + 290px)',
-        right: isMobile ? 'calc(8% + 30px)' : 'calc(10% + 510px)',
+        bottom: isMobile ? 'calc(15% + 170px)' : 'calc(18% + 280px)',
+        right: isMobile ? 'calc(8% + 30px)' : 'calc(10% + 360px)',
         zIndex: 999,
         width: `${outerSize}px`,
         height: `${outerSize}px`,
